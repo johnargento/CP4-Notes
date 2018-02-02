ba41630b15adb3aeba714af76514f1bc0751903e
johnargento <john.argento19@alumni.loyola.ca>
2018-02-02 13:55:09 -0500

    Create Schedule.js

---
+++ Schedule.js
@@ -0,0 +1,41 @@
+switch
+
+case 0: 
+	class = "English"
+	break;
+case 1:
+	class = "French"
+	break;
+case 2:
+	class = "ComputerProgramming"
+	break;
+case 3:
+	class = "Gym"
+	break;
+case 4
+	class = "Architecture"
+	break;
+case 5
+	class = "Science"
+	break;
+case 6
+	class = "History"
+	break;
+case 7
+	class = "Religion"
+	break;
+case 8
+	class = "LA4"
+	break;
+case 9
+	class = "Speech"
+	break;
+case 10
+	class = "Homeroom"
+	break;
+case 11
+	class = "Gym"
+	break;
+case 12
+	class = "N/A"
+	break;
